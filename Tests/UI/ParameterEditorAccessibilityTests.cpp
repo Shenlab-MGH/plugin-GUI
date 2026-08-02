7a24ae3b849e467c523233637d39804484288ad5
Cong <7380929+robotlearning123@users.noreply.github.com>
2026-08-02 06:36:25 -0400

    test: activate Linux combo focus host

--- Tests/UI/ParameterEditorAccessibilityTests.cpp
+++ Tests/UI/ParameterEditorAccessibilityTests.cpp
@@ -632,6 +632,7 @@ TEST_F (ParameterEditorAccessibilityTests,
 
     TrackingMessageThreadComboBox comboBox;
     TextButton sibling ("Sibling");
+    sibling.setWantsKeyboardFocus (true);
     comboBox.setEditableText (true);
     comboBox.addItem ("OFF", 1);
     comboBox.addItem ("-100", 2);
@@ -647,6 +648,8 @@ TEST_F (ParameterEditorAccessibilityTests,
     sibling.setBounds (160, 20, 80, 25);
     host.addAndMakeVisible (comboBox);
     host.addAndMakeVisible (sibling);
+    host.toFront (true);
+    MessageManager::getInstance()->runDispatchLoopUntil (10);
     auto* handler = comboBox.getAccessibilityHandler();
     ASSERT_NE (handler, nullptr);
 
